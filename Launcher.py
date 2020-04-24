8bf14e836e7a2bb4a2915dbf18ab1fc93281e798
Hillernz <58633959+Hillernz@users.noreply.github.com>
2020-04-24 17:14:52 +0300

    Lab1Python
    Ready to check!

--- Launcher.py
+++ Launcher.py
@@ -1,13 +1,20 @@
 import LoadingUtils
 import ImageUtils
 from PIL import Image
+import ModuleTests
+from datetime import datetime
+
 
 
 def main():
-    print("Working?")
-    LoadingUtils.load("https://www.thepythoncode.com/topic/web-scraping", "test-images")
-    print(ImageUtils.testPil("test-images/link-extractor.png"))
+    with open("urls.xml", "rt") as urls:
+        for url in urls:
+            LoadingUtils.load(url, "test-images")
+        ImageUtils.sortByImageBrightness("test-images")
 
+    #ModuleTests.testsForBrightness("module-test-images")
 
 if __name__ == '__main__':
+    start_time = datetime.now()
     main()
+    print("Total time = ", str(datetime.now() - start_time))
